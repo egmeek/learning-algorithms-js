688bb96a9214c02dc91b321035ab00db95a6cc78
reesington <reesington@users.noreply.github.com>
2015-12-06 10:37:01 -0600

    Add canvas support

--- som.js
+++ som.js
@@ -1,3 +1,9 @@
 /**
  * @author reesington / codepen.io/reesington
  */
+
+var canvas = document.getElementById('som'),
+		context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth / 3;
+canvas.height = window.innerWidth / 3;
